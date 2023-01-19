56ad2790736d37274dba715ce2ebfac6e25e9d86
Syam Sundar Boina <sb22aaz@herts.ac.uk>
2023-01-19 23:55:54 +0530

    first commit

---
+++ clustering and fitting.py
@@ -0,0 +1,6 @@
+#importing important libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.optimize as opt
+from scipy.optimize import curve_fit
+import sklearn.cluster as cluster
